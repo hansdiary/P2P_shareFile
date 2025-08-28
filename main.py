72b25e609f4334363f79fe92e9126e4c63c00323
Diary <diaryraoelison508@gmail.com>
2025-08-28 08:25:18 +0300

    Ajout méhode startClient()

--- main.py
+++ main.py
@@ -16,4 +16,16 @@ def startServer(host="127.0.0.1", port=5000, filename="noms.txt"):
             conn.sendall(data)
         print("Fichier envoyé avec suucès !!!")
         conn.close()    
-        
+def startClient(server_host="127.0.0.1", server_port=5000, output_file="downloaded.txt"):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_host, server_port))
+    print(f"[CLIENT] Connecté à {server_host}:{server_port}")
+    
+    with open(output_file, "wb") as f:
+        while True:
+            data = client_socket(1024)
+            if not data:
+                break
+            f.write(data)
+    print(f"[CLIENT] Fichier téléchargé et sauvegardé en `{output_file}`")
+    client_socket.close()        
